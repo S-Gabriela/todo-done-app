99dba2a78110c049a39cef138ad81d6bce015510
S-Gabriela <156372730+S-Gabriela@users.noreply.github.com>
2024-10-26 23:03:19 -0400

    Update server.js
    Server was updated for deployment

--- backend/server.js
+++ backend/server.js
@@ -86,6 +86,12 @@ app.delete('/tasks/:id', isAuthenticated, async (req, res) => {
         res.status(500).send('Error deleting task');
     }
 });
+const path = require('path');
 
+app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html'));
+});
 // Start server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
